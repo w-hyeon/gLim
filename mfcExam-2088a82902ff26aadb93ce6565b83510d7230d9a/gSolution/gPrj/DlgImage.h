33f45f3625fd6c375009a8f609101c91a9d9fc81
Woo Hyeon <115104658+w-hyeon@users.noreply.github.com>
2023-10-31 18:21:37 +0900

    tatat
    atatat

---
+++ mfcExam-2088a82902ff26aadb93ce6565b83510d7230d9a/gSolution/gPrj/DlgImage.h
@@ -0,0 +1,38 @@
+#pragma once
+
+
+// CDlgImage 대화 상자입니다.
+
+class CDlgImage : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlgImage)
+
+public:
+	CDlgImage(CWnd* pParent = NULL);   // 표준 생성자입니다.
+	virtual ~CDlgImage();
+
+	CWnd* m_pParent;
+	CImage m_image;
+	int m_nDataCount = 0;
+	CPoint m_ptData[100];
+
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLGIMAGE };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnUpParent();
+	virtual BOOL OnInitDialog();
+
+private:
+	void InitImage();
+	void drawData(CDC* pDC);
+public:
+	afx_msg void OnPaint();
+};
